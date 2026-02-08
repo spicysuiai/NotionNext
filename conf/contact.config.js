30ccb9cf24ac7003d2df495a76147fe36d4054f4
spicysuiai <8mdyh5bmxx@privaterelay.appleid.com>
2026-02-08 20:37:54 +0800

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -8,7 +8,7 @@ module.exports = {
       btoa(
         unescape(encodeURIComponent(process.env.NEXT_PUBLIC_CONTACT_EMAIL))
       )) ||
-    '25120242201585@stu.xmu.edu.cn', // 邮箱地址 例如mail@tangly1024.com
+    '2473107977@qq.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
