7c0e4930d0d56144d165cc1dc4156946798c6e0b
spicysuiai <8mdyh5bmxx@privaterelay.appleid.com>
2026-02-11 13:52:48 +0800

    Update comment.config.js

--- conf/comment.config.js
+++ conf/comment.config.js
@@ -16,14 +16,21 @@ module.exports = {
     'https://cdnjs.cloudflare.com/ajax/libs/artalk/2.5.5/Artalk.css', // ArtalkServert css cdn
 
   // twikoo
-  COMMENT_TWIKOO_ENV_ID: process.env.NEXT_PUBLIC_COMMENT_ENV_ID || 'twikoo-prod-2g4850o34a63be53',
-  COMMENT_TWIKOO_ENV_ID: process.env.NEXT_PUBLIC_COMMENT_ENV_ID || 'twikoo-prod-2g4850o34a63be53',
- // TWIKOO后端地址 腾讯云环境填envId；Vercel环境填域名，教程：https://tangly1024.com/article/notionnext-twikoo
-  COMMENT_TWIKOO_COUNT_ENABLE:
-    process.env.NEXT_PUBLIC_COMMENT_TWIKOO_COUNT_ENABLE || false, // 博客列表是否显示评论数
-  COMMENT_TWIKOO_CDN_URL:
-    process.env.NEXT_PUBLIC_COMMENT_TWIKOO_CDN_URL ||
-    'https://s4.zstatic.net/npm/twikoo@1.3.0/dist/twikoo.min.js', // twikoo客户端cdn
+// twikoo
+    // Twikoo backend URL (Vercel / custom domain), must start with https://
+    COMMENT_TWIKOO_ENV_ID:
+      process.env.NEXT_PUBLIC_COMMENT_ENV_ID ||
+      process.env.NEXT_PUBLIC_COMMENT_TWIKOO_ENV_ID ||
+      process.env.COMMENT_TWIKOO_ENV_ID ||
+      '',
+    
+    COMMENT_TWIKOO_COUNT_ENABLE:
+      process.env.NEXT_PUBLIC_COMMENT_TWIKOO_COUNT_ENABLE || false,
+    
+    COMMENT_TWIKOO_CDN_URL:
+      process.env.NEXT_PUBLIC_COMMENT_TWIKOO_CDN_URL ||
+      'https://s4.zstatic.net/npm/twikoo@1.6.44/dist/twikoo.min.js',
+// twikoo客户端cdn
 
   // utterance
   COMMENT_UTTERRANCES_REPO:
